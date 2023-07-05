93e13f7ca917436815b4ca21e8dff0bdcaec1f4a
Alessandra Maffei <ales.maffei@gmail.com>
2023-07-05 18:55:32 +0200

    memoized factorial

--- frontendmasters/recursion.js
+++ frontendmasters/recursion.js
@@ -21,10 +21,40 @@ function joinElementsLoop(array, start) {
   let resultSoFar = start;
 
   for (let i = 0; i < array.length; i++) {
-    return (resultSoFar += array[i]);
+    resultSoFar += ` ${array[i]}`;
   }
 
   return resultSoFar;
 }
 
 joinElementsLoop(["the", "good", "dog"], "elio");
+
+// factorial
+
+function factorial(n) {
+  if (n === 0) {
+    return n;
+  }
+  return n * factorial(n - 1);
+}
+
+function memoizeFactorial() {
+  const cache = new Map();
+
+  return function (n) {
+    if (cache.has(n)) {
+      console.log("another time");
+
+      return cache.get(n);
+    } else {
+      console.log("first time");
+      let result = factorial(n);
+      cache.set(n, result);
+      return result;
+    }
+  };
+}
+
+const memoizedFactorial = memoizeFactorial();
+memoizedFactorial(4);
+memoizedFactorial(4);
