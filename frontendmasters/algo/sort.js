ad3c380c7ad19c1d89e5e977f203b277821de48d
Alessandra Maffei <ales.maffei@gmail.com>
2023-07-09 08:07:39 +0200

    merge sort + bubble sort (frontend masters)

--- frontendmasters/algo/sort.js
+++ frontendmasters/algo/sort.js
@@ -1,19 +1,3 @@
-//bubble sort O(n2)
-
-function bubble(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
-    }
-  }
-
-  return arr;
-}
-
-bubble([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
-
 //merge sort O(nlogn)
 
 function merge(left, right) {
@@ -55,3 +39,19 @@ function mergeSort(arr) {
 
 mergeSort([9, 2, 5, 6, 4, 3, 7, 10, 1, 8]);
 mergeSort([9, 2, 5, 4]);
+
+//bubble sort O(n2)
+
+function bubble(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+bubble([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
